bc20dc6ec0721f34d7d2b94169a2cd92044ab896
Marcel Hoyer <mhoyer@pixelplastic.de>
2016-03-16 20:54:16 +0100

    Improves specs for createCard duck to verify card description

--- src/kanban/card.ducks.spec.ts
+++ src/kanban/card.ducks.spec.ts
@@ -3,7 +3,8 @@ import {createDispatchedActions, createReducer} from 'redux-typed-ducks';
 import {cardDucks} from './card.ducks';
 import {KanbanState} from '../types';
 
-let prevState = Immutable({
+let prevState: KanbanState;
+const initState: KanbanState = Immutable({
     boards: [{
         title: '',
         columns: [{
@@ -13,32 +14,34 @@ let prevState = Immutable({
     }],
     activeBoard: 0
 });
-const cardReducer = createReducer(cardDucks, prevState);
+const cardReducer = createReducer(cardDucks, initState);
 
 describe('Card reducers', () => {
     describe('Creating an initial first card', () => {
-        const newCard = {title: 'new Card'};
+        const newCard = { title: 'new Card', description: 'new Card Description' };
 
         it('adds card to the list', () => {
             const createAction = cardDucks.create({boardId: 0, columnId: 0, newCard});
-            const nextState = cardReducer(prevState, createAction);
+            const nextState = cardReducer(initState, createAction);
+            const nextCards = nextState.boards[0].columns[0].cards;
 
-            expect(nextState.boards[0].columns[0].cards.length).toBe(1);
-            expect(nextState.boards[0].columns[0].cards[0].title).toBe('new Card');
+            expect(nextCards.length).toBe(1);
+            expect(nextCards[0].title).toBe('new Card');
+            expect(nextCards[0].description).toBe('new Card Description');
         });
 
         it('keeps the previous state when board id is out of range', () => {
             const createAction = cardDucks.create({boardId: 1, columnId: 0, newCard});
-            const nextState = cardReducer(prevState, createAction);
+            const nextState = cardReducer(initState, createAction);
 
-            expect(nextState).toBe(prevState);
+            expect(nextState).toBe(initState);
         });
 
         it('keeps the previous state when column id is out of range', () => {
             const createAction = cardDucks.create({boardId: 0, columnId: 1, newCard});
-            const nextState = cardReducer(prevState, createAction);
+            const nextState = cardReducer(initState, createAction);
 
-            expect(nextState).toBe(prevState);
+            expect(nextState).toBe(initState);
         });
     });
 
@@ -46,14 +49,15 @@ describe('Card reducers', () => {
         const firstCard = {title: 'first'};
         const newCard = {title: 'new Card'};
 
-        it('adds column to the end of the list', () => {
-            prevState = prevState.setIn(['boards', 0, 'columns', 0, 'cards'], [firstCard]);
+        it('adds card to the end of the list', () => {
+            prevState = initState.setIn(['boards', 0, 'columns', 0, 'cards'], [firstCard]);
 
             const createAction = cardDucks.create({boardId: 0, columnId: 0, newCard});
             const nextState = cardReducer(prevState, createAction);
+            const nextCards = nextState.boards[0].columns[0].cards;
 
-            expect(nextState.boards[0].columns[0].cards.length).toBe(2);
-            expect(nextState.boards[0].columns[0].cards[1].title).toBe('new Card');
+            expect(nextCards.length).toBe(2);
+            expect(nextCards[1].title).toBe('new Card');
         });
     });
 });
