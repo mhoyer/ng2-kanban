9c733885493b5296a790838062c52085658b7a56
Marcel Hoyer <mhoyer@pixelplastic.de>
2016-03-19 01:15:00 +0100

    Extracts SidebarComponent from KanbanComponent

---
+++ src/components/sidebar.component.ts
@@ -0,0 +1,36 @@
+import {Component, ChangeDetectionStrategy, Input} from 'angular2/core';
+import {Store} from '@ngrx/store';
+import {KanbanState, Board} from '../types';
+import KanbanActions from '../kanban/kanban.duck';
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'sidebar',
+    template: `
+        <h1>Angular 2 - Kanban</h1>
+        <button (click)="createBoard()">Create Board</button>
+        <ul>
+            <li *ngFor="#board of boards">
+                <a href="#" (click)="selectBoard(board.id)">{{board.title}}</a>
+                <button (click)="deleteBoard(board.id)">Delete Board</button>
+            </li>
+        </ul>`,
+})
+export default class SidebarComponent {
+    @Input() boards: Board[];
+
+    constructor(private kanbanActions: KanbanActions) { }
+
+    createBoard() {
+        const newBoard = { title: `${this.boards.length + 1}. Board` };
+        this.kanbanActions.board.create(newBoard);
+    }
+
+    selectBoard(boardId: string) {
+        this.kanbanActions.board.select(boardId);
+    }
+
+    deleteBoard(boardId?: string) {
+        this.kanbanActions.board.delete(boardId);
+    }
+}
--- src/kanban/kanban.component.ts
+++ src/kanban/kanban.component.ts
@@ -1,45 +1,43 @@
 import {Component, ChangeDetectionStrategy} from 'angular2/core';
 import {Store} from '@ngrx/store';
 import {KanbanState} from '../types';
+import SidebarComponent from '../components/sidebar.component';
 import KanbanActions from './kanban.duck';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'kanban',
-    template: `<h1>Angular 2 - Kanban</h1>
-        <button (click)="createBoard()">Create Board</button>
-        <ul>
-            <li *ngFor="#board of state.boards">
-                <a href="#" (click)="selectBoard(board.id)">{{board.title}}</a>
-                <button (click)="deleteBoard(board.id)">Delete Board</button>
-            </li>
-        </ul>
-        <div *ngIf="selectedBoard">
-            <h3>{{selectedBoard.title}}</h3>
-            <input [value]="selectedBoard.title" #boardTitleEditor />
-            <button (click)="renameBoard(boardTitleEditor.value)">Rename</button>
-            <button (click)="deleteBoard()">Delete Board</button> |
-            <button (click)="createColumn()">Create Column</button>
-            <ul>
-                <li *ngFor="#column of columns">
-                    {{column.title}}
-                    <input [value]="column.title" #columnTitleEditor />
-                    <button (click)="renameColumn(column.id, columnTitleEditor.value)">Rename</button>
-                    <button (click)="deleteColumn(column.id)">Delete Column</button> |
-                    <button (click)="createCard(column.id)">Create Card</button>
-                    <ul>
-                        <li *ngFor="#card of cards(column.id)">
-                            <b>{{card.title}}</b> - {{card.description}}
-                            <input [value]="card.title" #cardTitleEditor />
-                            <input [value]="card.description" #cardDescriptionEditor />
-                            <button (click)="updateCard(card.id, cardTitleEditor.value, cardDescriptionEditor.value)">Update</button>
-                            <button (click)="deleteCard(card.id)">Delete</button>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <hr /><pre>{{debugState}}</pre>`,
+    directives: [SidebarComponent],
+    template: `
+        <sidebar [boards]="state.boards"></sidebar>
+        <board>
+            <div *ngIf="selectedBoard">
+                <h3>{{selectedBoard.title}}</h3>
+                <input [value]="selectedBoard.title" #boardTitleEditor />
+                <button (click)="renameBoard(boardTitleEditor.value)">Rename</button>
+                <button (click)="deleteBoard()">Delete Board</button> |
+                <button (click)="createColumn()">Create Column</button>
+                <ul>
+                    <li *ngFor="#column of columns">
+                        {{column.title}}
+                        <input [value]="column.title" #columnTitleEditor />
+                        <button (click)="renameColumn(column.id, columnTitleEditor.value)">Rename</button>
+                        <button (click)="deleteColumn(column.id)">Delete Column</button> |
+                        <button (click)="createCard(column.id)">Create Card</button>
+                        <ul>
+                            <li *ngFor="#card of cards(column.id)">
+                                <b>{{card.title}}</b> - {{card.description}}
+                                <input [value]="card.title" #cardTitleEditor />
+                                <input [value]="card.description" #cardDescriptionEditor />
+                                <button (click)="updateCard(card.id, cardTitleEditor.value, cardDescriptionEditor.value)">Update</button>
+                                <button (click)="deleteCard(card.id)">Delete</button>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <hr /><pre>{{debugState}}</pre>
+        </board>`,
 })
 export default class KanbanComponent {
     state: KanbanState;
@@ -51,18 +49,8 @@ export default class KanbanComponent {
         return JSON.stringify(this.state, null, 2);
     }
 
-    // board related
     get selectedBoard() {
         return this.state.boards.find(b => b.id === this.state.activeBoard);
-    }
-
-    createBoard() {
-        const newBoard = { title: `${this.state.boards.length + 1}. Board` };
-        this.kanbanActions.board.create(newBoard);
-    }
-
-    selectBoard(boardId: string) {
-        this.kanbanActions.board.select(boardId);
     }
 
     renameBoard(title) {
