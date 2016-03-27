70be1766fdf6d4643bd50d5af723df8f06133652
Marcel Hoyer <m.hoyer@cid.de>
2016-03-27 10:33:05 +0200

    Extracts KanbanHeaderComponent

---
+++ src/components/kanbanHeader.component.ts
@@ -0,0 +1,20 @@
+import {Component, ChangeDetectionStrategy} from 'angular2/core';
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'kanban-header',
+    template: `
+        <header>
+            <svg version="1.1" viewBox="0 0 500 500">
+                <g>
+                    <polygon fill="#B4B4B4" points="250,0 486,82 448,390    250,500   52,390 14,82"/>
+                    <polygon fill="#A51E22" points="250,26 463,99 430,374   250,474   70,250"/>
+                    <polygon fill="#DC2226" points="250,26                  250,474   70,374 37,99"/>
+                    <polygon fill="#B4B4B4" points="250,40 401,367 344,367 312,291 250,291 180,271 250,249 296,249 230,95"/>
+                    <polygon fill="#F3F3F3" points="250,40  99,367 156,367 186,291 250,291         250,249 204,249 250,140"/>
+                </g>
+            </svg>
+            <h1>Kanban</h1>
+        </header>`,
+})
+export default class KanbanHeaderComponent { }
--- src/components/sidebar.component.ts
+++ src/components/sidebar.component.ts
@@ -2,26 +2,15 @@ import {Component, ChangeDetectionStrategy, Input} from 'angular2/core';
 import {Store} from '@ngrx/store';
 import {KanbanState, Board} from '../types';
 import KanbanActions from '../kanban/kanban.ducks';
+import KanbanHeaderComponent from './kanbanHeader.component';
 import CreatorComponent from './creator.component';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'sidebar',
-    directives: [CreatorComponent],
+    directives: [KanbanHeaderComponent, CreatorComponent],
     template: `
-        <header>
-            <svg version="1.1" viewBox="0 0 500 500">
-                <g>
-                    <polygon fill="#B4B4B4" points="250,0 486,82 448,390    250,500   52,390 14,82"/>
-                    <polygon fill="#A51E22" points="250,26 463,99 430,374   250,474   70,250"/>
-                    <polygon fill="#DC2226" points="250,26                  250,474   70,374 37,99"/>
-                    <polygon fill="#B4B4B4" points="250,40 401,367 344,367 312,291 250,291 180,271 250,249 296,249 230,95"/>
-                    <polygon fill="#F3F3F3" points="250,40  99,367 156,367 186,291 250,291         250,249 204,249 250,140"/>
-                </g>
-            </svg>
-            <h1>Kanban</h1>
-        </header>
-        
+        <kanban-header></kanban-header>
         <creator (create)="createBoard($event)" placeholder="Title of new board"></creator>
 
         <ul>
