1bdfda0686826d84ff8cff6c2074d25b47488b12
Marcel Hoyer <m.hoyer@cid.de>
2016-04-07 23:37:00 +0200

    Adds initial KanbanHeaderComponent specs

---
+++ src/components/kanbanHeader.component.spec.ts
@@ -0,0 +1,8 @@
+import KanbanHeaderComponent from './kanbanHeader.component';
+
+describe('KanbanHeaderComponent', () => {
+    it('can be created', () => {
+        const sut = new KanbanHeaderComponent();
+        expect(sut).toBeDefined();
+    });
+});
